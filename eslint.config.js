ef69cb59ecee7163b4b8b22d0b51b81f92f32f5f
Martin Yrjölä <martin.yrjola@gmail.com>
2025-01-19 08:24:29 +0200

    Refactor and add prettier and eslint

---
+++ eslint.config.js
@@ -0,0 +1,5 @@
+import eslintPluginAstro from "eslint-plugin-astro";
+export default [
+  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginAstro.configs["jsx-a11y-recommended"],
+];
--- src/lib/cube.js
+++ src/lib/cube.js
@@ -4,6 +4,7 @@ export const cubeColorOffset = 4 * 4; // Byte offset of cube vertex color attrib
 export const cubeUVOffset = 4 * 8;
 export const cubeVertexCount = 36;
 
+// prettier-ignore
 export const cubeVertexArray = new Float32Array([
   // float4 position, float4 color, float2 uv,
   1, -1, 1, 1,   1, 0, 1, 1,  0, 1,
